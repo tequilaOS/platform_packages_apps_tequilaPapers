96d6b4ffbca29e1f95d5015594e756dbcc122820
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-10-29 18:52:03 +0100

    Update generate_smallvariants.py
    * We have only one resource directory now
    
    Change-Id: I64427d5c06d61a35d6e8b2ae8b8cae252efe2050

--- generate_smallvariants.py
+++ generate_smallvariants.py
@@ -5,9 +5,6 @@ from PIL import Image
 
 path = os.path.dirname(os.path.realpath(__file__))
 
-resources = ["res_1080p/drawable-nodpi",
-             "res_1440p/drawable-nodpi"]
-
 def generate_smallvariants(resource):
     global path
     wallpapers_path = os.path.join(path, resource)
@@ -34,5 +31,4 @@ def clean(wallpapers_path):
         if wallpaper.endswith("_small.jpg"):
             os.remove(os.path.join(wallpapers_path, wallpaper))
 
-for resource in resources:
-    generate_smallvariants(resource)
+generate_smallvariants("res/drawable-nodpi")
